0b326a0a5f16091f35dd84b0dc63ca905c40f9e7
Manan Mukim <mukimmanan@gmail.com>
2021-08-08 00:35:25 +0530

    Added Test To Check Negative Number Exception

--- src/testing/Tests.java
+++ src/testing/Tests.java
@@ -1,5 +1,7 @@
 package testing;
 import static org.junit.Assert.assertEquals;
+
+import mukimmanan.NegativeNumberException;
 import mukimmanan.StringAdd;
 import org.junit.Test;
 
@@ -10,32 +12,37 @@ public class Tests {
     }
 
     @Test
-    public void testingEmptyString() {
+    public void testingEmptyString() throws NegativeNumberException {
         assertEquals(0, StringAdd.add(""));
     }
 
     @Test
-    public void testingOneNumberString() {
+    public void testingOneNumberString() throws NegativeNumberException {
         assertEquals(2, StringAdd.add("2"));
     }
 
     @Test
-    public void testingTwoNumberString() {
+    public void testingTwoNumberString() throws NegativeNumberException {
         assertEquals(5, StringAdd.add("2,3"));
     }
 
     @Test
-    public void testingUnknownNumber() {
+    public void testingUnknownNumber() throws NegativeNumberException {
         assertEquals(15, StringAdd.add("1,2,3,4,5"));
     }
 
     @Test
-    public void testingNumberWithNewLine() {
+    public void testingNumberWithNewLine() throws NegativeNumberException {
         assertEquals(15, StringAdd.add("1\n2,3,4\n5"));
     }
 
     @Test
-    public void testingNumberWithNewDelimiter() {
+    public void testingNumberWithNewDelimiter() throws NegativeNumberException {
         assertEquals(4, StringAdd.add("//;\n1;3"));
+    }
+
+    @Test
+    public void testingNumberWithNegative() throws NegativeNumberException {
+        assertEquals(4, StringAdd.add("//;\n-1;3"));
     }
 }
