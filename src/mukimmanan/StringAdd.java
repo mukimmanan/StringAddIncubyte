69dc6118528b2978674567dc818522d613210a16
Manan Mukim <mukimmanan@gmail.com>
2021-08-07 23:29:10 +0530

    Added string add method (Which takes "", Single length string, any length string, but uses only 1st to digit for addition)

--- src/mukimmanan/StringAdd.java
+++ src/mukimmanan/StringAdd.java
@@ -1,4 +1,16 @@
 package mukimmanan;
 
 public class StringAdd {
+    public static int add(String text) {
+        if (text.equals("")) {
+            return 0;
+        }
+
+        if (text.length() == 1) {
+            return Integer.parseInt(text);
+        }
+
+        String[] values = text.split(",");
+        return Integer.parseInt(values[0]) + Integer.parseInt(values[1]);
+    }
 }
